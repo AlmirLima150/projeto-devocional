9cdb7445c1dce87b4bd973486858d88b17a8838c
sarachags <saraschagass@gmail.com>
2024-03-20 14:33:12 -0300

    Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
 import logo from "./assets/logo-simbolo.svg";
+import { CarDevocional } from "./components/card-devocional";
+
 
 
 export function App() {
   
 
   return (
-    <div className="mx-auto my-12 max-w-6xl space-y-6">
+    <div className="mx-auto my-12 max-w-7xl space-y-6">
 
       <div className="flex flex-1 items-center gap-2 justify-center">
       
         <img className="size-12" src={logo} alt="logo betania" />
-        <h1 className="text-3xl font-bold text-red-50  ">Devocionais</h1>
+        <h1 
+        className="text-3xl font-bold">
+          Devocionais
+        </h1>
 
       </div>
 
-      <div className="flex items-center">
-        <form className="w-full">
-          
-          <input 
-            type="text" 
-            placeholder="Busque em seus devocionais..." 
-            className="w-full bg-transparent text-2xl font-semibold tracking-tight outline-none placeholder:text-red-50 "
-            />
+      
+      <form className="w-full flex">
         
-        </form>
+        <input 
+          type="text" 
+          placeholder="Busque em seus devocionais..." 
+          className="w-full px-2 bg-red-50/20 rounded-lg text-2xl font-semibold tracking-tight outline-none placeholder:text-white"
+          />
+ 
+      </form>
+      
+      <div className="grid grid-cols-4 gap-3 auto-rows-[300px]">
+          <CarDevocional date={new Date()} content="hello word"/>
       </div>
 
     </div>
